6316620cf0dcd51e1a37d6f7fceb94a6fc089aab
Nick Harberg <nicholasharberg@gmail.com>
2019-08-19 12:46:35 -0700

    component life cycle

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 // Component
 // State
@@ -10,7 +11,9 @@ import './index.css'
 class App extends React.Component {
     render() {
         return (
-            <div>Hello Bilbo!</div>
+            <div className="container">
+                <Popular />
+            </div>
         )
     }
 }
